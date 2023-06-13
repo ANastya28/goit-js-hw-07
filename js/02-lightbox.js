bbaa8a025e6648c1e42efd2e27bdd9a9546c1dc7
ANastya28 <guselnikova2803@gmail.com>
2023-06-13 16:52:50 +0300

    Done 2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,9 +6,9 @@ console.log(galleryItems);
 
 const makeHtmlGallary = ({ preview, original, description }) => {
   return `<li class="gallery__item">
-            <a class="gallery__link" href="${original}">
+            <a class="gallery__image" href="${original}">
                <img
-                class="${preview}"
+                class="gallery__image"
                 src="${preview}"
                 alt="${description}"
                />
@@ -21,3 +21,16 @@ const gallery = galleryItems.map(makeHtmlGallary).join("");
 const galleryList = document.querySelector(`.gallery`);
 galleryList.insertAdjacentHTML("beforeend", gallery);
 
+galleryList.addEventListener(`click`, onImgContainerClick);
+  
+function onImgContainerClick(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== `IMG`) {
+        return;
+    };
+   
+   var lightbox = new SimpleLightbox('.gallery a', {captionDelay: 250, captionsData: 'alt' });
+   // console.log(lightbox);
+    
+};
